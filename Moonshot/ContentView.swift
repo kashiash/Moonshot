ed862f54c1d8c469278639698ffee8c7b5a035b4
kashiash <kashiash@gmail.com>
2022-07-21 22:46:01 +0200

    wprawki

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,8 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        GeometryReader{ geo in
+//   Image("scheffau")
+//            .resizable()
+//            .scaledToFit()
+//            .frame(width: geo.size.width * 0.8)
+//            .frame(width: geo.size.width, height: geo.size.height)
+//        }
+        
+//        ScrollView{
+//            LazyVStack(spacing: 10){
+//                ForEach(0..<100){
+//                    CustomText("Item \($0)")
+//                        .font(.title)
+//                }
+//            }
+//            .frame(maxWidth: .infinity)
+//        }
+       
+        
+        NavigationView{
+            List(0..<100){ row in
+            NavigationLink{
+                Text("Detail View \(row) ")
+            } label: {
+                Text("Row!\(row) ")
+                    .padding()
+                  
+            }
+            }
+
+        }
+        .navigationTitle("Dupa zbita !")
+        
+    }
+}
+
+struct CustomText: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
     }
 }
 
