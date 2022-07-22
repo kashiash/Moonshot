7f439b3dce7291b487c6329e05d0ce50a37992b5
kashiash <kashiash@gmail.com>
2022-07-22 22:11:40 +0200

    parsowanie jazona

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,53 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        GeometryReader{ geo in
-//   Image("scheffau")
-//            .resizable()
-//            .scaledToFit()
-//            .frame(width: geo.size.width * 0.8)
-//            .frame(width: geo.size.width, height: geo.size.height)
-//        }
-        
-//        ScrollView{
-//            LazyVStack(spacing: 10){
-//                ForEach(0..<100){
-//                    CustomText("Item \($0)")
-//                        .font(.title)
-//                }
-//            }
-//            .frame(maxWidth: .infinity)
-//        }
-       
-        
-        NavigationView{
-            List(0..<100){ row in
-            NavigationLink{
-                Text("Detail View \(row) ")
-            } label: {
-                Text("Row!\(row) ")
-                    .padding()
-                  
-            }
-            }
 
+        
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
         }
-        .navigationTitle("Dupa zbita !")
         
     }
 }
 
-struct CustomText: View {
-    let text: String
+struct User: Codable {
+    let name: String
+    let address: Address
+}
 
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
+struct Address: Codable {
+    let street: String
+    let city: String
 }
 
 struct ContentView_Previews: PreviewProvider {
